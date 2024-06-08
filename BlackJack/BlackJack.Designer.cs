7b5f21c827c2f70eb88324ddcf8127beb68f73fd
Панфилов Сергей Александрович <83750364+ZenArtZ98@users.noreply.github.com>
2024-06-08 17:30:42 +0500

    Update and rename Form1.Designer.cs to BlackJack.Designer.cs

--- BlackJack/BlackJack.Designer.cs
+++ BlackJack/BlackJack.Designer.cs
@@ -36,7 +36,7 @@ partial class Form1
         this.components = new System.ComponentModel.Container();
         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
         this.ClientSize = new System.Drawing.Size(1280, 720);
-        this.Text = "Form1";
+        this.Text = "BlackJack";
 
           
         Button btnStart = new Button();
